b32c63109062fd18a26cac83213950c74975dea6
pau13-loop <pllinas@cifpfbmoll.eu>
2022-02-06 20:58:25 +0100

    feat: :sparkles: Include navigation bar in partials directory

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,4 +6,13 @@
 
     <h1>Contact</h1>
 
+    <ul>
+        @forelse ($contactos as $contacto) 
+        {{-- Implementación de la variable LOOP --}}
+            <li>{{ $contacto['title'] }}<small>{{ $loop->first ? 'Es el primero' : '' }}</small></li>
+        @empty
+            <li>No hay proyectos para mostrar</li>
+        @endforelse
+    </ul>
+
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,17 +8,17 @@
 
     <!-- Le pasamos un segundo parámetro para que lo muestre por defecto en el caso de que no hayamos definido el título -->
     <title>@yield('title', 'Curso Aprendible')</title>
+    <style>
+        .active a {
+            color: red;
+            text-decoration: none;
+        }
+    </style>
 </head>
 
 <body>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-        </ul>
-    </nav>
+
+    @include('/partials/navigation')
 
     <!-- Agregar contenido dinámicamente con yield -->
     @yield('content')
